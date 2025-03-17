3ba5fdf99319a1e84a9aef5be88d95494bde7cd6
masashiono0611 <masashi.ono.0611@gmail.com>
2025-03-17 17:59:40 +0800

    feat: implement counter contract

--- tests/main.spec.ts
+++ tests/main.spec.ts
@@ -5,19 +5,26 @@ import { MainContract } from "../wrappers/MainContract";
 import "@ton/test-utils";
 
 describe("main.fc contract tests", () => {
-  it("should get the proper most recent sender address", async () => {
+  it("should successfully increase counter in contract and get the proper most recent sender address", async () => {
     const blockchain = await Blockchain.create();
     const codeCell = Cell.fromBoc(Buffer.from(hex, "hex"))[0];
 
-    const myContract = blockchain.openContract(
-      await MainContract.createFromConfig({}, codeCell)
-    );
-
     const senderWallet = await blockchain.treasury("sender");
 
-    const sentMessageResult = await myContract.sendInternalMessage(
+    const myContract = blockchain.openContract(
+      await MainContract.createFromConfig(
+        {
+          number: 0,
+          address: senderWallet.address,
+        },
+        codeCell
+      )
+    );
+
+    const sentMessageResult = await myContract.sendIncrement(
       senderWallet.getSender(),
-      toNano("0.05")
+      toNano("0.05"),
+      1
     );
 
     expect(sentMessageResult.transactions).toHaveTransaction({
@@ -29,5 +36,6 @@ describe("main.fc contract tests", () => {
     const data = await myContract.getData();
 
     expect(data.recent_sender.toString()).toBe(senderWallet.address.toString());
+    expect(data.number).toEqual(1);
   });
 });
